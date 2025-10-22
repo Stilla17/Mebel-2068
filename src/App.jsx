8c44cd5339a36a6e29fa989e992fc99f762a96e1
laziz711 <abdulazizmajidov515@gmail.com>
2025-10-22 14:43:32 +0500

    majidov

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Nav from './Components/Nav/Nav'
 
 const App = () => {
   return (
     <div>
-      
+      <Nav/>
     </div>
   )
 }
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
-
+import logo from './../../assets/logo.png';
 const Nav = () => {
   return (
-    <div>Nav</div>
+     <header className="bg-[#9dc82c] py-4">
+      <div className="max-w-7xl m-auto px-6 flex items-center justify-between">
+        <div className="flex items-center space-x-8">
+          <div className="flex items-center space-x-2">
+            <img src={logo} alt="logo" />
+          
+          </div>
+
+          <button className="flex items-center space-x-2 border border-white text-white px-4 py-1 rounded-md text-sm">
+            <span className="text-xl">:::</span>
+            <span>Каталог</span>
+          </button>
+
+          <nav className="hidden md:flex space-x-6 text-white text-sm">
+            <a href="#">Доставка</a>
+            <a href="#">Способы оплаты</a>
+            <a href="#">Контакты</a>
+            <a href="#">Гарантии и возврат</a>
+            <a href="#">Политика конфиденциальности</a>
+          </nav>
+        </div>
+
+        <div className="flex items-center space-x-2 text-white text-sm">
+          <span></span>
+          <span>Корзина</span>
+        </div>
+      </div>
+    </header>
+
   )
 }
 
